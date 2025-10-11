fbeb84b555ade44d8bb6493e671579502d52b684
uwiwiow <uwiwiow@gmail.com>
2025-10-10 22:15:14 -0600

    .

--- src/main.c
+++ src/main.c
@@ -2,9 +2,17 @@
 #include <raymath.h>
 #include "card.h"
 #include "debug.h"
+#include <stdio.h>
 
 constexpr int WIDTH = 900;
 constexpr int HEIGHT = 600;
+
+void AddCardToPile(Pile *pile, Card *card, int *CardIndex, Card *Deck) {
+	if (pile->card) {
+		AddCardToPile(pile, card, CardIndex, Deck);
+	}
+	
+}
 
 int main(int argc, char** argv) {
 
@@ -14,62 +22,58 @@ int main(int argc, char** argv) {
 	Texture2D SpadesAtlas = {};
 	LoadTextureCard(&SpadesAtlas, "res/Poker/Top-Down/Cards/Spades-88x124.png");
 
-	Card Deck[52] = {};
-	for (int i = 0; i < 52; i++) {
-		Deck[i] = (Card) {i % 13, true, {50 + (i % 13) * 60, 50 + (i / 13) * 130}};
+	const int DeckSize = 52;
+	Card Deck[DeckSize] = {};
+	for (int i = 0; i < DeckSize; i++) {
+		Deck[i] = (Card) {i % 13, true, {0, 0}};
 	}
-	
-	Card *Selected = nullptr;
 
-	Vector2 MousePosition = {0, 0};
-	bool MouseHold = false;
-	Vector2 MouseOffset = {0, 0};
+	const int PileSize = 7;
+	Pile Piles[PileSize] = {};
+	int CardIndex = 0;
+	for (int pile = 0; pile < PileSize; pile++) {
+		for (int pileAmount = 0; pileAmount <= pile; pileAmount++) {
+			Piles[pile] = (Pile){.card = &Deck[CardIndex], .next = nullptr};
+			Piles[pile].card->position = (Vector2) {50 + pile * 120, 50 + pileAmount * 50};
+			CardIndex++;
+			if (pile > 0) {
+				Piles[pile - 1].next = Piles[pile].card;
+			}
+		}
+	}
+
+	// const int StockSize = 24;
+	// Pile Stock = {};
+	// for (int pileAmount = 0; pileAmount < StockSize; pileAmount++) {
+	// 	Stock = (Pile){.card = &Deck[CardIndex], .next = nullptr};
+	// 	Piles[pile].card->position = (Vector2) {50 + pile * 120, 50 + pileAmount * 50};
+	// 	CardIndex++;
+	// 	if (pile > 0) {
+	// 		Piles[pile - 1].next = Piles[pile].card;
+	// 	}
+	// }
 
 	while(!WindowShouldClose()) {
 
 		Debug();
 
-		for (int i = 0; i < sizeof(Deck) / sizeof(Card); i++){
-			if (CheckCollisionPointRec(GetMousePosition(), (Rectangle) {Deck[i].position.x, Deck[i].position.y, 88, 124})) {
-				if (Deck[i].show){
-					Selected = &Deck[i];
-					break;
-				}
-			}
-			else {
-				if (!MouseHold)
-					Selected = nullptr;
-			}
-		}
-
-		if (IsMouseButtonDown(MOUSE_LEFT_BUTTON) && Selected) {
-			if (!MouseHold){
-				MouseOffset = Vector2Subtract(Selected->position, GetMousePosition());
-				MouseHold = true;
-			} 
-			Selected->position = Vector2Add(GetMousePosition(), MouseOffset);
-		}
-		
-		if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) && Selected) {
-			MouseHold = false;
-		}
 
 		BeginDrawing();
 
 		ClearBackground((Color) {51, 87, 171, 255});
 
-		for (int i = 0; i < sizeof(Deck) / sizeof(Card); i++){
+		for (int i = 0; i < DeckSize; i++){
 			DrawCard(SpadesAtlas, Deck[i]);
-			if (Selected){
-				if (Vector2Equals(Selected->position, Deck[i].position))
-					DrawCardBorder(SpadesAtlas, *Selected);
-			}
+			// if (Selected){
+			// 	if (Vector2Equals(Selected->position, Deck[i].position))
+			// 		DrawCardBorder(SpadesAtlas, *Selected);
+			// }
 			
 		}
 
 
 		DebugDraw(
-			LINE("%.0f\t%.0f", MousePosition.x, MousePosition.y),
+			// LINE("%.0f\t%.0f", MousePosition.x, MousePosition.y),
 			LINE("%.0f\t%.0f", GetMousePosition().x, GetMousePosition().y)
 		);
 
